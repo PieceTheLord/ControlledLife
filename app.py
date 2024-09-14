14cb41f95c3c2acd13e238e4a94cd48f20693522
ZXCBaron <alwx202@mail.ru>
2024-09-14 20:19:18 +0300

    added updating main page and displaying a current using app.

--- app.py
+++ app.py
@@ -1,12 +1,46 @@
+import flet_timer.flet_timer
 import win32gui
 import time
+import flet as ft
 
 def get_active_window_title():
     window_handle = win32gui.GetForegroundWindow()
     title = win32gui.GetWindowText(window_handle)
     return title
 
-while True:
-    active_window_title = get_active_window_title()
-    print(f"Active window title: {active_window_title}")
-    time.sleep(.5)
+def main(page: ft.Page):
+    page.title = "Title"
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+
+
+    txt_number = ft.Text(
+        get_active_window_title(),
+        text_align=ft.TextAlign.CENTER, 
+        width=400
+    )
+
+    def get_window_title():
+        txt_number.value = get_active_window_title(),
+        page.update()
+
+
+    page.add(
+        ft.Row(
+            [
+                # ft.IconButton(icon=ft.icons.REMOVE, on_click=minus_click),
+                txt_number,
+                # ft.IconButton(ft.icons.ADD, on_click=plus_click)
+            ],
+            alignment=ft.MainAxisAlignment.CENTER
+        )
+    )
+    timer = flet_timer.flet_timer.Timer("timer", 1, get_window_title)
+    page.add(timer)
+    
+
+ft.app(main)
+
+# while True:
+# active_window_title = get_active_window_title()
+# print(f"Active window title: {get_active_window_title()}")
+# time.sleep(.5)
