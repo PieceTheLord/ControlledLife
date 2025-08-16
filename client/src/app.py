e661f30efe479199033d96dd564fe23f30ff618e
PieceTheLord <alwx202@mail.ru>
2025-08-16 13:38:38 +0300

    Revert "worked classes decomposition"
    This reverts commit 9065ea1441ce836a1671967b6a1b5292331e79e4.
    
    Changed datetime module to time. And Some debug print

--- client/src/app.py
+++ client/src/app.py
@@ -1,11 +1,52 @@
+from datetime import datetime
+import time
+from tracker.Tracker import Window
+import threading
 import flet as ft
+from utils.time_divider import parse_string
 
-from imports import ActivityTrackerUI, ActivityTracker, Window_methods
 
-def app(page: ft.Page):
-    """Start the app, just for a beauty @main func"""
-    ui = ActivityTrackerUI(page)
-    tracker = ActivityTracker(page, ui)
-    test = Window_methods(page, ui)
-    tracker.ActivityTracker.start_tracking()
-    test.start_tracking()
+def main(page: ft.Page):
+
+    # Create the list of our tracking apps' title
+    lv = ft.ListView(expand=1, padding=20, spacing=10, auto_scroll=True)
+    page.title = "Activity tracker"
+    timer_running = True
+    count_time = datetime.now()
+    titles_list: list[str] = []
+
+    def update_timer():
+        while timer_running:
+            time.sleep(1)
+            add_active_window_title()
+            page.update()
+
+    def add_active_window_title():
+        nonlocal count_time
+        active_app_title = Window.get_active_title()
+        if len(lv.controls) == 0:
+            current_time = str(datetime.now() - count_time).split(".")[0]
+            titles_list.append(active_app_title)
+            lv.controls.append(ft.Text(active_app_title + " " + current_time))
+
+            count_time = datetime.now()
+
+        elif titles_list[-1] != active_app_title:
+            current_time = str(datetime.now() - count_time).split(".")[0]
+            lv.controls.append(ft.Text(titles_list[-1] + " " + current_time))
+            titles_list.append(active_app_title)
+
+            count_time = datetime.now()
+        else:
+            pass
+
+    page.add(lv)
+
+    timer_thread = threading.Thread(target=update_timer, daemon=True)
+    timer_thread.start()
+
+    page.update()
+
+
+if __name__ == "__main__":
+    ft.app(target=main)
