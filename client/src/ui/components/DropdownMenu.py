4843245d2b30f36d8d9da810c12fb400359fa3e2
PieceTheLord <alwx202@mail.ru>
2025-08-25 19:14:01 +0300

    Worked dropdown menu, ugly thouhg.

--- client/src/ui/components/DropdownMenu.py
+++ client/src/ui/components/DropdownMenu.py
@@ -66,22 +66,30 @@ class DropdownMenu:
             ]
         )
 
+    # Check the subtitle exists
     def check_mainTitle(self):
         if self.menu_data["subtitle1"]["title"] != "":
             return True
 
+    # Check the last title existst (e.g app.py or a vieo's title)
     def check_subtitle(self):
         if self.menu_data["subtitle1"]["subtitle2"]["title"] != "":
             return True
 
     def toggle_mainTitle(self, e):
-        if self.check_mainTitle():
+        if self.states["mainTitle_expanded"] == True:
+            self.states["mainTitle_expanded"] = False
+            self.states["subtitle_expanded"] = True
+        elif self.check_mainTitle():
             self.states["mainTitle_expanded"] = not self.states["mainTitle_expanded"]
+        # Rebuild IconButton, because self.content update doesn't rebuild the page based on code
         self.mainTitleButton.icon = (
             ft.Icons.ARROW_UPWARD_SHARP
             if self.states["mainTitle_expanded"]
             else ft.Icons.ARROW_DOWNWARD_SHARP
         )
+
+        # Rebuild the controls, because self.content update doesn't rebuild the page based on code
         self.subtitle.controls = [
             (
                 ft.Row(
@@ -98,6 +106,15 @@ class DropdownMenu:
             )
         ]
 
+        # Rebuild controls
+        self.lastTitle.controls = [
+            (
+                ft.Row([ft.Text(self.menu_data["subtitle1"]["subtitle2"]["title"])])
+                if self.states["subtitle_expanded"]
+                else ft.Row([])
+            )
+        ]
+
         self.update_menu()
 
     def toggle_subtitle(self, e):
@@ -107,16 +124,16 @@ class DropdownMenu:
             self.states["subtitle_expanded"],
             self.menu_data["subtitle1"]["subtitle2"]["title"],
         )
+        # Rebuild IconButton
         self.subtitleButton.icon = (
             ft.Icons.ARROW_UPWARD_SHARP
             if self.states["subtitle_expanded"]
             else ft.Icons.ARROW_DOWNWARD_SHARP
         )
+        # Rebuild last title control
         self.lastTitle.controls = [
             (
-                ft.Row(
-                    [ft.Text(self.menu_data["subtitle1"]["subtitle2"]["title"])]
-                )
+                ft.Row([ft.Text(self.menu_data["subtitle1"]["subtitle2"]["title"])])
                 if self.states["subtitle_expanded"]
                 else ft.Row([])
             )
