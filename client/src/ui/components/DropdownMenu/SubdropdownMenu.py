eac0f1d9f0b4f9a6fedb04dabb1a387e3e7d8e20
PieceTheLord <alwx202@mail.ru>
2025-10-15 01:53:10 +0300

    Base dropdown implemented

--- client/src/ui/components/DropdownMenu/SubdropdownMenu.py
+++ client/src/ui/components/DropdownMenu/SubdropdownMenu.py
@@ -11,27 +11,18 @@ class SubdropdownMenu:
             ft.Row([ft.Text(f"Total spent time - {session['total_spent_time']}")]),
         ]
         for subtitle, subtitle_data in session["subtitles"].items():
-            print(
-                f"subtitle",
-                subtitle,
-                "subtitle_data",
-                subtitle_data,
-                sep="\n",
-            )
+            # print(
+            #     f"subtitle",
+            #     subtitle,
+            #     "subtitle_data",
+            #     subtitle_data,
+            #     sep="\n",
+            # )
             self.content_controls.append(
                 lastDropdownMenu(
+                    title=subtitle,
                     session=subtitle_data,
-                    subtitle_expanded=self.states["subtitle_expanded"],
                 ).content
             )
-            if subtitle_data["subtitles"]:
-                for subtitle2, subtitle2_data in subtitle_data["subtitles"].items():
-                    print(
-                        "subtitle2",
-                        subtitle2,
-                        "subtitle2_data",
-                        subtitle2_data,
-                        sep="\n",
-                    )
 
         self.content = ft.Column(self.content_controls)
--- client/src/ui/components/DropdownMenu/lastDropdowMenu.py
+++ client/src/ui/components/DropdownMenu/lastDropdowMenu.py
@@ -2,14 +2,64 @@ import flet as ft
 
 
 class lastDropdownMenu:
-    def __init__(self, subtitle_expanded, session):
+    def __init__(self, title, session: dict):
         self.states = {
-            "subtitle_expanded": subtitle_expanded,
+            "subtitle_expanded": False,
         }
-        self.content_controls = []
+        self.session = session
+        self.lastTitle_button = ft.IconButton(
+            icon=(
+                ft.Icons.ARROW_UPWARD_SHARP
+                if self.states["subtitle_expanded"]
+                else ft.Icons.ARROW_DOWNWARD_SHARP
+            ),
+            on_click=self.toggle_lastTitle,
+        )
+        self.last_title = ft.Text(title)
         self.content = ft.Column(
             [
-                ft.Text(f"{subtitle} - {subtitle_data['total_spent_time']}")
-                for subtitle, subtitle_data in session["subtitles"].items()
+                ft.Row(
+                    [
+                        self.lastTitle_button,
+                        self.last_title,
+                    ]
+                ),
+                (
+                    ft.Column(
+                        [
+                            ft.Text(f"{subtitle} - {subtitle_data['total_spent_time']}")
+                            for subtitle, subtitle_data in session["subtitles"].items()
+                        ]
+                    )
+                    if self.states["subtitle_expanded"]
+                    else ft.Column([])
+                ),
             ]
         )
+
+    def toggle_lastTitle(self, e):
+        self.states["subtitle_expanded"] = not self.states["subtitle_expanded"]
+        print(self.states["subtitle_expanded"])
+        self.lastTitle_button.icon = (
+            ft.Icons.ARROW_UPWARD_SHARP
+            if self.states["subtitle_expanded"]
+            else ft.Icons.ARROW_DOWNWARD_SHARP
+        )
+        self.content.controls = [
+            ft.Row(
+                [
+                    self.lastTitle_button,
+                    self.last_title,
+                ]
+            ),
+            (
+                ft.Column(
+                    [
+                        ft.Text(f"{subtitle} - {subtitle_data['total_spent_time']}")
+                        for subtitle, subtitle_data in self.session["subtitles"].items()
+                    ]
+                )
+                if self.states["subtitle_expanded"]
+                else ft.Column([])
+            ),
+        ]
